14cf0bdb121f76897c231e947e574aad6d53d820
kangdaehoon <eogns47@konkuk.ac.kr>
2023-09-05 04:32:22 +0900

    start spring server

---
+++ src/main/java/mame/mameWeb/MameWebApplication.java
@@ -0,0 +1,13 @@
+package mame.mameWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MameWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MameWebApplication.class, args);
+	}
+
+}
